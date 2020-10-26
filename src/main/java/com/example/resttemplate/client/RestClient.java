9ff5a81a9a34e737ccc6782f94571671999c7ba1
DaniilNov <Daniil.1989>
2020-10-26 14:39:59 +0300

    Upd 1

--- src/main/java/com/example/resttemplate/client/RestClient.java
+++ src/main/java/com/example/resttemplate/client/RestClient.java
@@ -17,19 +17,10 @@ public class RestClient {
     private static String DELETE_USER = "http://91.241.64.178:7081/api/users/{id}";
 
     static RestTemplate restTemplate = new RestTemplate();
-    private static String Post;
-    private static String Put;
-    private static String Del;
-
-
     private static String Cookie;
 
     public static void main(String[] args) {
-
-
         getAllUsers();
-
-
     }
 
     private static void getAllUsers() {
@@ -44,28 +35,21 @@ public class RestClient {
         postUser();
         putUser();
         deleteUser();
-        System.out.println(Post + Put + Del);
-
-
+        System.out.println(postUser() + putUser() + deleteUser());
     }
 
-    private static void postUser() {
-
+    private static String postUser() {
         User user = new User(3L, "James", "Brown", (byte) 20);
-
         RequestEntity<User> requestEntity = RequestEntity
                 .post(URI.create(POST_USER))
                 .contentType(MediaType.APPLICATION_JSON)
                 .header("Cookie", Cookie)
                 .body(user);
         ResponseEntity<String> entity = restTemplate.exchange(requestEntity, String.class);
-        Post = entity.getBody();
-
-        System.out.println(entity);
+        return entity.getBody();
     }
 
-
-    private static void putUser() {
+    private static String putUser() {
         Long id = 3L;
         User updateUser = new User(id, "Thomas", "Shelby", (byte) 30);
         RequestEntity<User> requestEntity = RequestEntity
@@ -74,22 +58,18 @@ public class RestClient {
                 .header("Cookie", Cookie)
                 .body(updateUser);
         ResponseEntity<String> entity = restTemplate.exchange(requestEntity, String.class);
-        Put = entity.getBody();
-
-        System.out.println(entity);
+        return entity.getBody();
     }
 
-    private static void deleteUser() {
+    private static String deleteUser() {
         Map<String, Long> param = new HashMap<>();
         param.put("id", 3L);
         HttpHeaders headers = new HttpHeaders();
         headers.add("Cookie", Cookie);
         HttpEntity<String> httpEntity = new HttpEntity<>(headers);
         ResponseEntity<String> responseEntity = restTemplate.exchange(DELETE_USER, HttpMethod.DELETE, httpEntity, String.class, param);
-        Del = responseEntity.getBody();
-        System.out.println(responseEntity);
+        return responseEntity.getBody();
     }
-
 }
 
 
